bb8f693ae4c2fae308c4272aed1b2b3590852543
Walter Woshid <36635504+WalterWoshid@users.noreply.github.com>
2022-08-07 19:40:56 +0200

    Added 'noError' option to tasker and better type completions

--- lib/helper/tasker.ts
+++ lib/helper/tasker.ts
@@ -15,22 +15,27 @@ export class Tasker<
   TaskResult extends ReturnType<Task>,
 
   // Options
-  Options extends Partial<ITaskerOptions>,
+  Options extends Partial<ITaskerAllOptions<NoError, ThrowOnError, HasDistinctiveResult>>,
+  // Inferred option from the 'noError' option
+  NoError extends Options['noError'] extends true ? true : false,
+  // Inferred option from the 'throwOnError' option
+  ThrowOnError extends Options['throwOnError'] extends true ? true : false,
   // Inferred option from the 'distinctiveResults' option
   HasDistinctiveResult extends Options['distinctiveResults'] extends true ? true : false,
 > {
-  private readonly options: Options;
+  private readonly options: ITaskerAllOptions;
 
   constructor(
     private readonly elements: Element[],
     private readonly task: Task,
+    // Pass options with `as const` to prevent type errors
     options?: Options,
   ) {
     // Merge options
     this.options = {
       ...defaultTaskerOptions,
       ...options,
-    } as Options;
+    };
   };
 
   /**
@@ -71,39 +76,52 @@ export class Tasker<
     TaskSuccess extends Awaited<TaskResult>,
     // The result of a failed task
     TaskError extends Awaited<Error>,
- 
+
     // Typescript automatically infers the result type from a truthy/falsy check
     IsError extends true | false,
     CheckableError extends { isError: IsError },
- 
+
+    HasErrors extends boolean,
+    HasErrorsCondition extends NoError extends true
+      ? {}
+      : ThrowOnError extends true
+        ? {}
+        : {
+          // If the tasker has errors
+          hasErrors: HasErrors
+        },
+
     /**
      * @see ITaskerOptions.distinctiveResults
      */
     DistinctiveResult extends IsError extends false
       ? { error: TaskError } & { isError: true }
       : { value: TaskSuccess } & { isError: false },
- 
+
     /**
      * @see ITaskerOptions.distinctiveResults
      */
     NonDistinctiveResult extends IsError extends false
       ? { value: TaskError } & { isError: true }
       : { value: TaskSuccess } & { isError: false },
- 
+
     TaskerResult extends Record<
       Element | string | number | symbol,
-      (HasDistinctiveResult extends true
-        ? DistinctiveResult
-        : NonDistinctiveResult
-      )
+      NoError extends true
+        ? TaskSuccess
+        : ThrowOnError extends true
+          ? TaskSuccess
+          : HasDistinctiveResult extends true
+            ? DistinctiveResult
+            : NonDistinctiveResult
     >,
-  >(): Promise<{
-    // The result of the tasker
-    result: TaskerResult,
-    // If the tasker has errors
-    hasErrors: boolean,
-  }> {
-    let hasErrors = false;
+
+    RunResult extends {
+      // The result of the tasker
+      result: TaskerResult,
+    } & HasErrorsCondition
+  >(): Promise<RunResult> {
+    let hasErrors: boolean = false;
   
     const results = await Promise.all(
       this.elements.map(element => {
@@ -111,19 +129,19 @@ export class Tasker<
           try {
             const taskResult = await this.task(element) as TaskSuccess & CheckableError;
             taskResult.isError = false;
-  
+
             resolve({
               [element]: taskResult,
             });
           } catch (e) {
             e = e as TaskError & CheckableError;
- 
+
             if (this.options.throwOnError) {
               throw e;
             } else {
               hasErrors = true;
               e.isError = true;
- 
+
               return resolve({
                 [element]: e
               });
@@ -132,31 +150,68 @@ export class Tasker<
         });
       })
     );
- 
+
     // Squash results into a single object
-    const result = Object.assign({}, ...results);
- 
+    const result: TaskerResult = Object.assign({}, ...results);
+
     return {
       result,
-      hasErrors,
-    };
+      hasErrors: (this.options.noError || this.options.throwOnError)
+        ? undefined
+        : hasErrors as HasErrors,
+    } as any;
   }
 }
 
-const defaultTaskerOptions: ITaskerOptions = {
+const defaultTaskerOptions: ITaskerAllOptions = {
+  noError: false,
   throwOnError: false,
   distinctiveResults: false,
 };
 
-type ITaskerOptions = {
+interface ITaskerAllOptions<
+  NoError = boolean,
+  ThrowOnError = boolean,
+  DistinctiveResults = boolean
+> {
   /**
-   * If true, throw the error instead of adding it to the result.
+   * Disable error handling.
    *
    * @default false
    */
-  throwOnError: boolean;
+  noError: NoError;
 
   /**
+   * If true, throw the error instead of adding it to the result.
+   *
+   *
+   * <b>If true:</b>
+   *
+   * ```
+   * result = {
+   *   [element]: Result,
+   * }
+   * ```
+   *
+   * <b>If false:</b>
+   * ```
+   * result = {
+   *   [element]: {
+   *     isError: boolean,
+   *     value: Result | Error,
+   *   }
+   * }
+   * ```
+   *
+   * @default false
+   *
+   * @constraint If 'noError' is true, this option is ignored.
+   */
+  throwOnError: NoError extends true ? never : ThrowOnError;
+
+  /**
+   * Whether to create the property 'value' and 'error' separately or combine them into a single property 'value'.
+   *
    * <b>If true:</b>
    *
    * ```
@@ -183,6 +238,12 @@ type ITaskerOptions = {
    * ```
    *
    * @default false
+   *
+   * @constraint If 'noError' or 'throwOnError' is true, this option is ignored.
    */
-  distinctiveResults: boolean;
+  distinctiveResults: NoError extends true
+    ? never
+    : ThrowOnError extends true
+      ? never
+      : DistinctiveResults;
 }
